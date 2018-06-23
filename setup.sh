a141b4f5ebe8bb9bc8945d5a06a891589425275d
Ole Hansen <ole@jlab.org>
2018-06-23 19:27:51 -0400

    Expect data files in $WORKSHOP_DATA, defaulting to /data if undefined

--- setup.sh
+++ setup.sh
@@ -13,6 +13,25 @@ else
     fi
 fi
 
+[ -z "$WORKSHOP_DATA" ] && export WORKSHOP_DATA=/data
+
+if [ ! -e ${THIS}/data ]; then
+    if [ ! -z "$WORKSHOP_DATA" -a -r ${WORKSHOP_DATA}/raw ]; then
+	ln -s ${WORKSHOP_DATA}/raw ${THIS}/data
+    else
+	echo "$WORKSHOP_DATA/raw not found, creating empty data dir"
+	mkdir ${THIS}/data
+    fi
+fi
+if [ ! -e ${THIS}/rootfiles ]; then
+    if [ ! -z "$WORKSHOP_DATA" -a -r ${WORKSHOP_DATA}/ROOTfiles ]; then
+	ln -s ${WORKSHOP_DATA}/ROOTfiles ${THIS}/rootfiles
+    else
+	echo "$WORKSHOP_DATA/ROOTfiles not found, creating empty rootfiles dir"
+	mkdir ${THIS}/rootfiles
+    fi
+fi
+
 export DB_DIR=${THIS}/DB
 export DATA_DIR=${THIS}/data
 export OUT_DIR=${THIS}/rootfiles
